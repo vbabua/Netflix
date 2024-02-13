86ab663f22626d78d3fb437b30dcb15308e988f1
yoonpaix <babuvadakemu@gmail.com>
2024-02-13 19:15:16 +0000

    config: Update Django settings for PostgreSQL database connection
    - Modified DATABASES setting in settings.py to configure PostgreSQL as the default database.
    - Included necessary database connection details (host, port, database name, user, and password).

--- netflix/netflix/settings.py
+++ netflix/netflix/settings.py
@@ -75,9 +75,15 @@ WSGI_APPLICATION = "netflix.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default':{
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME' : '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST' : '',
+        'PORT' : '5432',
+
+
     }
 }
 
